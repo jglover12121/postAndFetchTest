a824e09439a9737d1c83259807d2a4d76c7b7677
jglover12121 <57909497+jglover12121@users.noreply.github.com>
2024-07-24 18:17:09 +0000

    addedMoreFunctionallity

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ fetch('https://example.com/data')
   .then(data => console.log(data)) 
   .catch(error => console.error(error)); 
 
+const oriHref = window.location.href;
+
+console.log(oriHref);
+const roomCode = 0;
 
 document.getElementById("setRoom").addEventListener("click", ()=>{
-  window.location.href = window.location.href + toString(document.getElementById("roomCodeInput"))
+  roomCode = toString(document.getElementById("roomCodeInput"))
+  window.location.href = oriHref + roomCode;
+})
+document.getElementById("leaveRoom").addEventListener("click", ()=>{
+  window.location.href = oriHref;
 })
