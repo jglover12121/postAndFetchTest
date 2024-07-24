faf9c7ba40f8d7ada575c83e3b653a803efbeba8
jglover12121 <57909497+jglover12121@users.noreply.github.com>
2024-07-24 18:01:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('https://example.com/data') 
+  .then(response => response.json()) 
+  .then(data => console.log(data)) 
+  .catch(error => console.error(error)); 
+
+
+document.getElementById("setRoom").addEventListener("click", ()=>{
+  window.location.href = 
+})
